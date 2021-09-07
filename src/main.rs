bf60403f520054818e70f0f60a27dcf00dcffbd4
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-09-07 17:54:21 -0400

    add draw function for the grid

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ const HEADING_ANGLE: f32 = 1.0;
 const SENSE_ANGLE: f32 = 1.0;
 const SENSE_DISTANCE: f32 = 1.0;
 const TURN_ANGLE: f32 = 1.0;
+const DEPOSIT_AMOUNT: f32 = 0.7;
 
 struct Particle {
     pos: Vector2,
@@ -61,12 +62,36 @@ impl Grid {
         }
     }
 
-    pub fn update(&self) {
-        // TODO
+    pub fn cell_at(&self, row: usize, col: usize) -> &Cell {
+        &self.cells[(row * WIDTH) + col]
     }
 
-    pub fn draw(&self) {
+    pub fn cell_at_mut(&mut self, row: usize, col: usize) -> &mut Cell {
+        &mut self.cells[(row * WIDTH) + col]
+    }
+
+    pub fn update(&self) {
         // TODO
+        // 1. Perform a blur on the trail array
+    }
+
+    pub fn draw(&self, app: &App, model: &Model, frame: &Frame, draw: &Draw) {
+        let width = WIDTH as u32;
+        let height = HEIGHT as u32;
+        let image = nannou::image::ImageBuffer::from_fn(width, height, |x, y| {
+            let cell = self.cell_at(x as usize, y as usize);
+            let color = map_range(clamp(cell.intensity, 0.0, 1.0), 0.0, 1.0, 0, std::u8::MAX);
+
+            nannou::image::Rgba([color, color, color, std::u8::MAX])
+        });
+        let flat_samples = image.as_flat_samples();
+        model.texture.upload_data(
+            app.main_window().swap_chain_device(),
+            &mut *frame.command_encoder(),
+            &flat_samples.as_slice(),
+        );
+
+        draw.texture(&model.texture);
     }
 }
 
@@ -74,6 +99,7 @@ struct Model {
     _window: WindowId,
     grid: Grid,
     particles: Vec<Particle>,
+    texture: wgpu::Texture,
 }
 
 fn main() {
@@ -81,29 +107,41 @@ fn main() {
 }
 
 fn model(app: &App) -> Model {
+    let width = WIDTH as u32;
+    let height = HEIGHT as u32;
+
     let _window = app
         .new_window()
-        .size(WIDTH as u32, HEIGHT as u32)
+        .size(width, height)
         .view(view)
         .build()
         .unwrap();
 
     let grid = Grid::new(WIDTH, HEIGHT);
-    let particles = (0..100).map(|_| Particle::random()).collect();
+    let particles = (0..WIDTH * HEIGHT).map(|_| Particle::random()).collect();
+    let texture = wgpu::TextureBuilder::new()
+        .size([width, height])
+        .format(wgpu::TextureFormat::Rgba8Unorm)
+        .usage(wgpu::TextureUsage::COPY_DST | wgpu::TextureUsage::SAMPLED)
+        .build(app.main_window().swap_chain_device());
 
     Model {
         _window,
         grid,
         particles,
+        texture,
     }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     draw.background().color(BLACK);
+
+    // Paint the grid!
+    model.grid.draw(app, model, &frame, &draw);
 
     draw.to_frame(app, &frame).unwrap();
 }
