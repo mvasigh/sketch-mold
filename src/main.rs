04ce831d5ac0013051ba14e4e60797898d38d859
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2022-05-03 13:32:27 -0400

    inverse circle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
+#[macro_use]
+extern crate lazy_static;
+
+use lerp::Lerp;
 use nannou::prelude::*;
+use rand_distr::{Distribution, Normal};
 
 const SCALE: usize = 2;
 const SCALE_F32: f32 = SCALE as f32;
+const SCALE_DIST: f32 = 0.75;
 const WIDTH: usize = 400 * SCALE;
 const HEIGHT: usize = 400 * SCALE;
-const NUM_PARTICLES: usize = 60000 * SCALE;
-const HEADING_DISTANCE: f32 = 1.125 * SCALE_F32;
+const NUM_PARTICLES: usize = 200000 * SCALE;
+const HEADING_DISTANCE: f32 = 1.125 * SCALE_F32 * SCALE_DIST;
 const SENSE_ANGLE: f32 = PI * 0.5;
-const SENSE_DISTANCE: f32 = 1.25 * SCALE_F32;
+const SENSE_DISTANCE: f32 = 1.25 * SCALE_F32 * SCALE_DIST;
 const TURN_ANGLE: f32 = PI * 0.25;
-const DEPOSIT_AMOUNT: f32 = 0.25;
-const DECAY_AMOUNT: f32 = 0.068;
+const DEPOSIT_AMOUNT: f32 = 0.31;
+const DECAY_AMOUNT: f32 = 0.065;
 const BLUR_RADIUS: isize = 1;
 const MIN_CUTOFF: f32 = 0.1;
 const MAX_CUTOFF: f32 = 0.8;
+const BLUR_INTENSITY: f32 = 0.35;
 
-const MAX_COLOR: [u8; 3] = [0, 0, 0];
-const MIN_COLOR: [u8; 3] = [255, 255, 255];
+const MAX_COLOR: [u8; 3] = [139, 77, 219];
+const MIN_COLOR: [u8; 3] = [252, 33, 73];
 
-const IMG_OUTPUT: bool = false;
+const IMG_OUTPUT: bool = true;
 
 fn cart_to_canvas(pt: Vector2) -> Vector2 {
     let x = pt.x + (WIDTH as f32 / 2.0);
@@ -51,6 +58,7 @@ fn inf_coords(x: isize, y: isize) -> usize {
     index as usize
 }
 
+#[derive(Debug)]
 struct Particle {
     pos: Vector2,
     heading_angle: f32,
@@ -58,6 +66,7 @@ struct Particle {
     sense_angle: f32,
     sense_distance: f32,
     turn_angle: f32,
+    color: [f32; 3],
 }
 
 impl Particle {
@@ -69,6 +78,7 @@ impl Particle {
             sense_angle: SENSE_ANGLE,
             sense_distance: SENSE_DISTANCE,
             turn_angle: TURN_ANGLE,
+            color: [1.0, 1.0, 1.0],
         }
     }
 
@@ -80,12 +90,25 @@ impl Particle {
     }
 
     pub fn center(radius: f32) -> Particle {
-        let r = random_range(0.0, radius);
+        lazy_static! {
+            static ref NORMAL: Normal<f32> = Normal::new(0.0, 0.4).unwrap();
+        }
+
+        let v = NORMAL.sample(&mut rand::thread_rng());
+        let r = map_range(1.0 - v.abs(), 0.0, 1.0,radius / 4.0, radius);
         let a = random_range(0.0, 2.0 * PI);
         let dx = r * f32::cos(a);
         let dy = r * f32::sin(a);
 
         Particle::new((WIDTH / 2) as f32 + dx, (HEIGHT / 2) as f32 + dy, a)
+    }
+
+    pub fn set_color(&mut self, color: [u8; 3]) {
+        self.color = [
+            map_range(color[0] as f32, 0.0, std::u8::MAX as f32, 0.0, 1.0),
+            map_range(color[1] as f32, 0.0, std::u8::MAX as f32, 0.0, 1.0),
+            map_range(color[2] as f32, 0.0, std::u8::MAX as f32, 0.0, 1.0),
+        ]
     }
 
     pub fn update(&mut self, grid: &mut Grid) {
@@ -112,14 +135,15 @@ impl Particle {
         let right_cell = grid.cell_at_pt(right_pt);
 
         // 3. Determine the new heading angle based on the sensors
-        if left_cell.intensity > center_cell.intensity && left_cell.intensity > right_cell.intensity
+        if left_cell.concentration() > center_cell.concentration()
+            && left_cell.concentration() > right_cell.concentration()
         {
             self.heading_angle -= self.turn_angle;
-        } else if right_cell.intensity > center_cell.intensity
-            && right_cell.intensity > left_cell.intensity
+        } else if right_cell.concentration() > center_cell.concentration()
+            && right_cell.concentration() > left_cell.concentration()
         {
             self.heading_angle += self.turn_angle;
-        } else if left_cell.intensity == right_cell.intensity {
+        } else if left_cell.concentration() == right_cell.concentration() {
             match random_f32() > 0.5 {
                 true => self.heading_angle -= self.turn_angle,
                 false => self.heading_angle += self.turn_angle,
@@ -128,7 +152,7 @@ impl Particle {
 
         // 4. Deposit some pheromone at current location
         let curr_cell = grid.cell_at_pt_mut(self.pos);
-        curr_cell.deposit();
+        curr_cell.deposit(self);
 
         // 5. Move particle to new destination
         self.pos = cart_to_canvas(move_coords(
@@ -156,16 +180,31 @@ impl Particle {
 
 #[derive(Clone, Debug, Copy)]
 struct Cell {
-    intensity: f32,
+    intensity: [f32; 3],
 }
 
 impl Cell {
     pub fn decay(&mut self) {
-        self.intensity = clamp(self.intensity - DECAY_AMOUNT, 0.0, 1.0);
+        for i in 0..3 {
+            self.intensity[i] = clamp(self.intensity[i] - DECAY_AMOUNT, 0.0, 1.0);
+        }
     }
 
-    pub fn deposit(&mut self) {
-        self.intensity = clamp(self.intensity + DEPOSIT_AMOUNT, 0.0, 1.0);
+    pub fn deposit(&mut self, particle: &Particle) {
+        for i in 0..3 {
+            let deposit_amt = particle.color[i] * DEPOSIT_AMOUNT;
+            self.intensity[i] = clamp(self.intensity[i] + deposit_amt, 0.0, 1.0);
+        }
+    }
+
+    pub fn concentration(&self) -> f32 {
+        map_range(
+            self.intensity[0] + self.intensity[1] + self.intensity[2],
+            0.0,
+            3.0,
+            0.0,
+            1.0,
+        )
     }
 }
 
@@ -179,7 +218,8 @@ impl Grid {
     pub fn new(width: usize, height: usize) -> Grid {
         let cells = (0..width * height)
             .map(|_| Cell {
-                intensity: clamp(random_f32(), 0.0, 0.2),
+                // TODO
+                intensity: [0.0; 3],
             })
             .collect();
 
@@ -246,38 +286,60 @@ impl Grid {
 
     fn blur_horizontal(&mut self, dest: &mut Vec<Cell>, radius: isize) {
         for y in 0..self.height as isize {
-            let mut total: f32 = 0.0;
+            for i in 0..3 {
+                let mut total: f32 = 0.0;
 
-            for kx in -radius..radius + 1 {
-                total += self.cells[inf_coords(kx, y)].intensity;
-            }
+                for kx in -radius..radius + 1 {
+                    total += self.cells[inf_coords(kx, y)].intensity[i];
+                }
 
-            dest[inf_coords(0, y)].intensity = total / ((radius * 2 + 1) as f32);
+                // dest[inf_coords(0, y)].intensity[i] = total / ((radius * 2 + 1) as f32);
 
-            for x in 1..self.width as isize {
-                total -= self.cells[inf_coords(x - radius - 1, y)].intensity;
-                total += self.cells[inf_coords(x + radius, y)].intensity;
+                dest[inf_coords(0, y)].intensity[i] = Lerp::lerp(
+                    dest[inf_coords(0, y)].intensity[i],
+                    total / ((radius * 2 + 1) as f32),
+                    BLUR_INTENSITY,
+                );
 
-                dest[inf_coords(x, y)].intensity = total / ((radius * 2 + 1) as f32);
+                for x in 1..self.width as isize {
+                    total -= self.cells[inf_coords(x - radius - 1, y)].intensity[i];
+                    total += self.cells[inf_coords(x + radius, y)].intensity[i];
+
+                    dest[inf_coords(x, y)].intensity[i] = Lerp::lerp(
+                        dest[inf_coords(x, y)].intensity[i],
+                        total / ((radius * 2 + 1) as f32),
+                        BLUR_INTENSITY,
+                    );
+                }
             }
         }
     }
 
     fn blur_vertical(&mut self, dest: &mut Vec<Cell>, radius: isize) {
         for x in 0..self.width as isize {
-            let mut total: f32 = 0.0;
+            for i in 0..3 {
+                let mut total: f32 = 0.0;
 
-            for ky in -radius..radius + 1 {
-                total += self.cells[inf_coords(x, ky)].intensity;
-            }
+                for ky in -radius..radius + 1 {
+                    total += self.cells[inf_coords(x, ky)].intensity[i];
+                }
 
-            dest[inf_coords(x, 0)].intensity = total / ((radius * 2 + 1) as f32);
+                dest[inf_coords(x, 0)].intensity[i] = Lerp::lerp(
+                    dest[inf_coords(x, 0)].intensity[i],
+                    total / ((radius * 2 + 1) as f32),
+                    BLUR_INTENSITY,
+                );
 
-            for y in 1..self.height as isize {
-                total -= self.cells[inf_coords(x, y - radius - 1)].intensity;
-                total += self.cells[inf_coords(x, y + radius)].intensity;
+                for y in 1..self.height as isize {
+                    total -= self.cells[inf_coords(x, y - radius - 1)].intensity[i];
+                    total += self.cells[inf_coords(x, y + radius)].intensity[i];
 
-                dest[inf_coords(x, y)].intensity = total / ((radius * 2 + 1) as f32);
+                    dest[inf_coords(x, y)].intensity[i] = Lerp::lerp(
+                        dest[inf_coords(x, y)].intensity[i],
+                        total / ((radius * 2 + 1) as f32),
+                        BLUR_INTENSITY,
+                    );
+                }
             }
         }
     }
@@ -291,25 +353,25 @@ impl Grid {
             let max = MAX_CUTOFF;
 
             let r = map_range(
-                clamp(cell.intensity, min, max),
+                clamp(cell.intensity[0], min, max),
                 min,
                 max,
-                MIN_COLOR[0],
-                MAX_COLOR[0],
+                std::u8::MIN,
+                std::u8::MAX,
             );
             let g = map_range(
-                clamp(cell.intensity, min, max),
+                clamp(cell.intensity[1], min, max),
                 min,
                 max,
-                MIN_COLOR[1],
-                MAX_COLOR[1],
+                std::u8::MIN,
+                std::u8::MAX,
             );
             let b = map_range(
-                clamp(cell.intensity, min, max),
+                clamp(cell.intensity[2], min, max),
                 min,
                 max,
-                MIN_COLOR[2],
-                MAX_COLOR[2],
+                std::u8::MIN,
+                std::u8::MAX,
             );
 
             nannou::image::Rgba([r, g, b, std::u8::MAX])
@@ -348,7 +410,34 @@ fn model(app: &App) -> Model {
         .unwrap();
 
     let grid = Grid::new(WIDTH, HEIGHT);
-    let particles = (0..NUM_PARTICLES).map(|_| Particle::center(WIDTH as f32 * 0.45)).collect();
+    let particles = (0..NUM_PARTICLES)
+        .map(|_| {
+            let mut particle = Particle::center(WIDTH as f32 * 0.45);
+            let width = WIDTH as f32;
+            let height = HEIGHT as f32;
+            let x_val = if particle.pos.x < 0.0 {
+                width + particle.pos.x
+            } else {
+                particle.pos.x % width
+            };
+            let y_val = if particle.pos.y < 0.0 {
+                width + particle.pos.y
+            } else {
+                particle.pos.y % width
+            };
+
+            let val = random_f32();
+            let max = 1.0;
+
+            particle.set_color([
+                map_range(val, 0.0, max, MIN_COLOR[0], MAX_COLOR[0]),
+                map_range(val, 0.0, max, MIN_COLOR[1], MAX_COLOR[1]),
+                map_range(val, 0.0, max, MIN_COLOR[2], MAX_COLOR[2]),
+            ]);
+
+            particle
+        })
+        .collect();
     let texture = wgpu::TextureBuilder::new()
         .size([width, height])
         .format(wgpu::TextureFormat::Rgba8Unorm)
